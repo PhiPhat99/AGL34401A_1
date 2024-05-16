e1103a4486ac1d66a4a162a6070fec90921c9800
LAPTOP-5QSR3R04\HP <ppman2702@gmail.com>
2024-05-16 22:24:08 +0700

    Add Project...

---
+++ Src/Agilent 34401A Multimeter_Test/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agilent_34401A_Multimeter_Test
+{
+    public partial class Form1 : Form
+    {
+        bool meterRemoteStare = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public delegate void AddDataDelegate(string Agilent34401A_Date);
+        public AddDataDelegate myDelegate;
+
+        public AddDataMethod(string Data)
+        {
+            textBox1.AppendText("Received data - " + Data + "\n");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!meterRemoteStare)
+            {
+                meterRemoteStare = true;
+                button1.Text = "LOCAL";
+                button1.BackColor = Color.LightYellow;
+                if (!(meterRemoteStare.IsOpen)) 
+                {
+                    serialPort1.Open();
+                    serialPort1.Write("SYST:REM\n");
+                }
+            }
+            else
+            {
+                meterRemoteStare = false;
+                button1.Text = "Remote";
+                button1.BackColor = Color.Red;
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Write("SYST:LOC\n");
+                    serialPort1.Close();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Write("MEAS:VOLT:DC? 10\n");
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            String Data(serialPort1.ReadLine());
+            textBox1.Invoke(this.myDelegate, new object[] { Data });
+        }
+    }
+}
